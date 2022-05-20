2fe61618d2600f2964586ca33cf2cf00be14f010
GJMShrigley <gjmshrigley@gmail.com>
2022-05-20 18:53:11 +0100

    Implemented drag and drop functionality to Notes Component. Implemented Save functionality to Notes Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import FileSaver, { saveAs } from 'file-saver';
 
 import Title from "./components/Title";
 import Note from "./components/Note";
 
 import { BsPlusLg } from "react-icons/bs";
+import { BsSave } from "react-icons/bs";
 
 import './style.css';
-
 export default function App() {
-  
-  const [NotesData, setNotesData] = React.useState([{id: 0, 
-    placeNo: 0,
+
+  const [count, setCount] = React.useState(1);
+
+  const [NotesData, setNotesData] = React.useState([{
+    id: 0, 
+    index: 0,
+    position: `item 0`,
     text:"",
     checked: false,
-    isEdit: true}]) 
+    isEdit: true,
+  }]) 
 
 function addNote() {
+
+  setCount(prevCount => prevCount + 1);
+
   setNotesData(prevState => {
     return [...prevState , { 
-      id: prevState.length, 
-      placeNo: prevState.length,
+      id: count, 
+      index: count,
+      position: `item ${count}`,
       text:"",
       checked: false,
-      isEdit: true
+      isEdit: true,
       }]
   })
 }
@@ -56,33 +67,64 @@ function handleCheck(id) {
   for (let i = 0; i < NotesDataCopy.length; i++) {
     if (NotesDataCopy[i].id === noteId) {
       NotesDataCopy[i].checked = !NotesDataCopy[i].checked;
-      setNotesData(NotesDataCopy)
+      setNotesData(NotesDataCopy);
     }
   }
 }
 
+function handleOnDragEnd(result) {
+  if (!result.destination) return;
+  const NotesDataCopy = Array.from(NotesData);
+  const [reorderedNotes] = NotesDataCopy.splice(result.source.index, 1); 
+  NotesDataCopy.splice(result.destination.index, 0, reorderedNotes);
+  setNotesData(NotesDataCopy);
+}
+
+function saveFile() {
+  const notesText = JSON.stringify(NotesData);
+  const file = new File([notesText], "Notes.txt", {type: "text/plain;charset=utf-8"});
+  FileSaver.saveAs(file);
+}
+
   const notes = NotesData.map(item => {
     return (
-      <Note
-        key= {item.id}
-        id= {item.id}
-        remove={removeNote}
-        text={handleText}
-        checked={handleCheck}
-        />
+      <Draggable key={item.id} draggableId={item.position} index={item.index}>
+      {(provided) => (
+        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <Note
+            key= {item.id}
+            id= {item.id}
+            remove={removeNote}
+            text={handleText}
+            checked={handleCheck}
+          />
+        </div>
+        )}
+        </Draggable>
     )
   }) 
   
   return (
     <div>
       <Title />
-      <div className="add-remove-buttons">
-      <div className="button" onClick={console.log(NotesData)}></div>
-        <div className="button" id="add-button" onClick={addNote}><BsPlusLg /></div>
-      </div>      
-      <section className="notes-list">
-        {notes}
-      </section>   
+      <div className="buttons-container">
+        <div className="add-remove-buttons">
+          <div className="button" id="add-button" onClick={addNote}><BsPlusLg /></div>
+        </div> 
+        <div className="save-load-buttons">
+          <div className="button" id="save-button" onClick={saveFile}><BsSave /></div>
+        </div>
+      </div>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="droppable-notes">
+            {(provided) => (
+              <section className="notes-list" {...provided.droppableProps} ref={provided.innerRef}>
+                {notes}
+                {provided.placeholder}
+              </section>
+            )}
+          </Droppable>
+        </DragDropContext> 
     </div>    
   );
 }
